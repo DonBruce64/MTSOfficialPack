ed2492a0e3ae2d96c240f5d27cc094bf543bdea5
don_bruce <rapscallion827@gmail.com>
2018-06-30 17:18:12 -0500

    Moved instruments into pack system.

--- main/java/mtsofficialpack/MTSPackLoader.java
+++ main/java/mtsofficialpack/MTSPackLoader.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
  * Should you be compiling this for your own use, any compiler from 1.10.2 onward should compile it with no issues.
  * 
  * Note that the "required-after" can also be used to require other mods besides MTS.
- * This is incredibly useful if your pack depends on another content pack, or a mod that's not MTS
+ * This is useful if your pack depends on another content pack, or a mod that's not MTS
  * (such as Immersive Engineering for steel, for example). 
  * 
  * @author don_bruce
@@ -48,6 +48,7 @@ public class MTSPackLoader{
 			Class packParserSystem = Class.forName("minecrafttransportsimulator.systems.PackParserSystem");
 			Method addMultipartMethod = packParserSystem.getMethod("addMultipartDefinition", InputStreamReader.class, String.class, String.class);
 			Method addPartMethod = packParserSystem.getMethod("addPartDefinition", InputStreamReader.class, String.class, String.class);
+			Method addInstrumentMethod = packParserSystem.getMethod("addInstrumentDefinition", InputStreamReader.class, String.class, String.class);
 			
 			String className = this.getClass().getSimpleName();
 			String packageName = this.getClass().getPackage().getName();
@@ -58,28 +59,31 @@ public class MTSPackLoader{
 			classDir = classDir.substring(0, classDir.indexOf('!'));
 			
 			/*From here on out the system should search the jarfile this class is located in and find all the files
-			In the assets/<MODID>/jsondefs/vehicles section.  It should take all files located in there and send
-			them to the addMultipartMethod.  The same should happen for parts until all of them are loaded.
-			The String parameter in both cases is the MODID, and should match the folder/package name here.
+			In the assets/<MODID>/jsondefs section.  It should take all files located in there and send
+			them to the appropriate method in the PackParserSystem.
+			The String parameter in all cases is the MODID, and should match the folder/package name here.
 			Make sure the entries are sorted, however, as the parser can't do that itself.
 			*/
 			ZipFile jarFile = new ZipFile(classDir);
 			Enumeration<? extends ZipEntry> entries = jarFile.entries();
 			List<String> multipartEntries = new ArrayList<String>();
 			List<String> partEntries = new ArrayList<String>();
+			List<String> instrumentEntries = new ArrayList<String>();
 			
 			while(entries.hasMoreElements()){
 				ZipEntry entry = entries.nextElement();		
-				if(entry.getName().endsWith(".json")){					 
+				if(entry.getName().endsWith(".json")){
 					if(entry.getName().contains("jsondefs/vehicles")){
 						multipartEntries.add(entry.getName());
 					}else if(entry.getName().contains("jsondefs/parts")){
 						partEntries.add(entry.getName());
+					}else if(entry.getName().contains("jsondefs/instruments")){
+						instrumentEntries.add(entry.getName());
 					}
 				}
 			}
 			
-			//Sort the the entries, and then send the off to be registered.
+			//Sort the the entries, and then send them off to be registered.
 			multipartEntries.sort(null);
 			for(String entryName : multipartEntries){
 				String entryFileName = entryName.substring(entryName.lastIndexOf('/') + 1, entryName.length() - ".json".length());
@@ -89,6 +93,11 @@ public class MTSPackLoader{
 			for(String entryName : partEntries){
 				String entryFileName = entryName.substring(entryName.lastIndexOf('/') + 1, entryName.length() - ".json".length());
 				addPartMethod.invoke(null, new InputStreamReader(jarFile.getInputStream(jarFile.getEntry(entryName))), entryFileName, MODID);
+			}
+			instrumentEntries.sort(null);
+			for(String entryName : instrumentEntries){
+				String entryFileName = entryName.substring(entryName.lastIndexOf('/') + 1, entryName.length() - ".json".length());
+				addInstrumentMethod.invoke(null, new InputStreamReader(jarFile.getInputStream(jarFile.getEntry(entryName))), entryFileName, MODID);
 			}
 			jarFile.close();
 		}catch(Exception e){
